a319b43ffd1a4fe91e2db570b60aff7038e349b2
jiieea <seigneur.noir15@gmail.com>
2026-09-11 21:53:07 +0700

    Encapsulation demo

---
+++ src/Encapsulation_Demo/Main.java
@@ -0,0 +1,15 @@
+package Encapsulation_Demo;
+
+public class Main {
+    private String email;
+
+
+//    getter method to access the atribute
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String paramEmail) {
+        this.email = paramEmail;
+    }
+}
---
+++ src/Encapsulation_Demo/Test.java
@@ -0,0 +1,9 @@
+package Encapsulation_Demo;
+
+public class Test {
+    public static void main(String[] args) {
+        Main objectMineGweh = new Main();
+        objectMineGweh.setEmail("test@outlook.com");
+        System.out.println(objectMineGweh.getEmail());
+    }
+}
